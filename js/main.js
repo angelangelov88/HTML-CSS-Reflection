ad2ad4b650096d939baed1cdfa803e7393e37e68
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2021-07-12 12:18:03 +0100

    Added sticky header and navbar
    I added some JS to make the header and the navbar sticky. They disappear with scrolling down and appear again when user scrolls back up. I had issues with some of the elements on the page as the new positioning of the header and navbar misplaced my brand image and the header for some screens. I fixed the CSS in order to make it work and it all works fine on all browsers and screens

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// This function has been added to make the header and navbar sticky. They disappear when srolling down and appear again on scroll up 
+(function(){
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.querySelector('.header-container');
+  var navbarHeader = document.querySelector('.main-navigation');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+    
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 205) { 
+      
+      //replace 52 with the height of your header in px
+
+      header.classList.add('hide');
+      navbarHeader.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      navbarHeader.classList.remove('hide');
+
+      prevDirection = direction;
+    }
+  };
+
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
+
+
+
+
